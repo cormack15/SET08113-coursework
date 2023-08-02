691d3c616c3a819bb72db0f956c394a53e4859d2
Travis Cormack <40175268@live.napier.ac.uk>
2023-08-02 03:20:29 +0100

    Added Country class and SQL statement

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,47 +1,119 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class App {
-    public static void main(String[] args) {
-        try {
+public class App
+{
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e)
+        {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
-        for (int i = 0; i < retries; ++i) {
-            System.out.println("Connecting to the database...");
-            try {
-                // Wait a bit for the db to start
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
                 Thread.sleep(30000);
-                // Connect to the database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected to the database");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
                 break;
-            } catch (SQLException sqle) {
-                System.out.println("Failed to connect to the database (attempt " + Integer.toString(i) + ")");
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
+            }
+            catch (InterruptedException ie)
+            {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
-        if (con != null) {
-            try {
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
                 // Close connection
                 con.close();
-            } catch (Exception e) {
-                System.out.println("Error closing connection to the database");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public List<Country> getCountriesByPopulation()
+    {
+        List<Country> countries = new ArrayList<>();
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create the SQL query string to retrieve country information ordered by population
+            String strSelect = "SELECT code, name, population "
+                    + "FROM country "
+                    + "ORDER BY population DESC";
+
+            // Execute the SQL statement and retrieve the result set
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Iterate through the result set and populate the list of countries
+            while (rset.next()) {
+                Country country = new Country();
+                country.code = rset.getString("code");
+                country.name = rset.getString("name");
+                country.population = rset.getInt("population");
+                countries.add(country);
+            }
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country information");
+        }
+        return countries;
+    }
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,33 @@
+package com.napier.sem;
+
+public class Country {
+    /**
+     * Country's code
+     */
+    public String code;
+
+    /**
+     * Country's name
+     */
+    public String name;
+
+    /**
+     * Country's Continent
+     */
+    public String continient;
+
+    /**
+     * Country's Region
+     */
+    public String region;
+
+    /**
+     *
+     */
+    public int population;
+
+    /**
+     * Country's Capital
+     */
+    public String capital;
+}
